62572705f607ecd2af95752ce84b58a849150386
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-18 09:26:24 +0100

    enmity port test

---
+++ test/src/index.tsx
@@ -0,0 +1,29 @@
+import { Plugin, registerPlugin } from 'enmity/managers/plugins';
+import { getByProps } from 'enmity/metro';
+import { create } from 'enmity/patcher';
+import manifest from '../manifest.json';
+import show from './spotifyactionsheet';
+
+const Patcher = create('spotify-preview');
+
+const handleClick = getByProps("handleClick");
+
+const SilentTyping: Plugin = {
+   ...manifest,
+
+   onStart() {
+      Patcher.before(handleClick, "handleClick", function ([args]) {
+         const { href } = args;
+         const isSpotify = href.startsWith("https://open.spotify.com/");
+         if (!isSpotify) return;
+         args.href = undefined;
+         show(href);
+     });
+   },
+
+   onStop() {
+      Patcher.unpatchAll();
+   }
+};
+
+registerPlugin(SilentTyping);
---
+++ test/src/spotifyactionsheet.tsx
@@ -0,0 +1,41 @@
+import { Toasts, Constants } from "enmity/metro/common"
+import { getModule, getByName, getByProps } from 'enmity/metro';
+import { React } from 'enmity/metro/common';
+
+const { ThemeColorMap } = Constants;
+
+const { default: { render: ActionSheet } } = (getModule(x => x.default?.render?.name == "ActionSheet") ?? { default: { render: false } });
+const LazyActionSheet = getByProps("openLazy", "hideActionSheet");
+
+// https://discord.com/channels/1015931589865246730/1062531774187573308/1117197626648039494 — Thanks Rosie on Vendetta Discord
+function renderActionSheet(component: any, props: { [key: string]: any }) {
+  ActionSheet
+    ? LazyActionSheet?.openLazy(new Promise(r => r({ default: component })), "ActionSheet", props)
+    : Toasts.open("You cannot open ActionSheets on this version! Upgrade to 163+");
+};
+const { BottomSheetScrollView } = getByProps("BottomSheetScrollView");
+
+let WebView = getByName("WebView");
+let wv = (link)=>{ 
+    const bgcolor = ThemeColorMap.MODAL_BACKGROUND;
+    return (
+        <WebView
+          source={{ uri: link }}
+          style={{ marginTop: 20, backgroundColor: bgcolor, height: 100, width: "100%" }}
+        />
+      );
+};
+
+function as({'0': link}) {
+    return (
+        <ActionSheet>
+            <BottomSheetScrollView contentContainerStyle={{ marginBottom: 100 }}>
+            {wv(link.replace('https://open.spotify.com', 'https://open.spotify.com/embed'))}
+            </BottomSheetScrollView>
+        </ActionSheet>
+        );
+}
+
+export default function show(url) {
+  renderActionSheet(as, [url]);
+};
